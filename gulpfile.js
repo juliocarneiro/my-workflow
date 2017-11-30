93e25d3b76a6a0478a502137c4f393db4ea83d4c
juliocarneiro <juliocarneiro@gmail.com>
2017-11-30 18:28:20 -0200

    atualização

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,15 @@
 var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    imagemin    = require('gulp-imagemin'),
-    changed     = require('gulp-changed'),
-    browserSync = require('browser-sync');
- 
+sass        = require('gulp-sass'),
+changed     = require('gulp-changed'),
+browserSync = require('browser-sync');
+
 gulp.task('sass', function () {
-    gulp.src('./sass/style.scss')
-        .pipe(sass({compass: true}))
-        .on('error', function (err) { console.log(err.message); })
-        .pipe(gulp.dest('./css'));
+gulp.src('./sass/style.scss')
+    .pipe(sass({compass: true}))
+    .on('error', function (err) { console.log(err.message); })
+    .pipe(gulp.dest('./css'));
 });
- 
-gulp.task('jpg', function() {
-    gulp.src('./img/**/*.jpg')
-        .pipe(changed('./img/'))
-        .pipe(imagemin({
-            progressive: true
-        }))
-        .pipe(gulp.dest('./img/'));
-});
- 
+
 gulp.task('browser-sync', function() {
     browserSync.init(['./*.html', './css/*.css', './js/*.js'], {
         server: {
@@ -28,7 +18,7 @@ gulp.task('browser-sync', function() {
         }
     });
 });
- 
+
 gulp.task('watch', ['sass', 'browser-sync'], function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,3 @@
 (function(){
-	$('nav a, .quadro').mouseover(function(){
-		$('.quadro').stop().fadeIn('fast');
-	});
-	$('nav a, .quadro').mouseleave(function(){
-		$('.quadro').stop().fadeOut('fast');
-	});
+	
 })();
